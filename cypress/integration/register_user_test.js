c07b04bc118837760b6f98783092418b3311eeeb
Nadeera Abeykoon <nabeykoon@gmail.com>
2021-09-18 20:19:20 +0530

    Update test with API test to login using token

--- cypress/integration/register_user_test.js
+++ cypress/integration/register_user_test.js
@@ -5,34 +5,75 @@ describe("Register new user in Juice shop", () => {
     let randomString = Math.random().toString(36).substring(2);
 
     let username = "user-" + randomString;
-    let email = randomString + "@gmail.com";
-    let password = "Password1";
-    let maidenName = "Wije"
+    const email = randomString + "@gmail.com";
+    const password = "Password1";
+    const maidenName = "Wije"
 
-    beforeEach(() => {
-        cy.visit("http://localhost:3000/");
-        cy.get(".cdk-overlay-backdrop").click({ force: true });
-        cy.get("#navbarAccount").click();
-        cy.get("#navbarLoginButton").click();
+    
+    describe("UI tests", () => {
+        beforeEach(() => {
+            cy.visit("http://localhost:3000/");
+            cy.get(".cdk-overlay-backdrop").click({ force: true });
+            cy.get("#navbarAccount").click();
+            cy.get("#navbarLoginButton").click();
+        })
+    
+        it("Sign up new user", () => {
+            cy.get("#newCustomerLink").click();
+            cy.get("#emailControl").type(email);
+            cy.get("#passwordControl").type(password);
+            cy.get("#repeatPasswordControl").type(password);
+            cy.get(".mat-select-placeholder").click();
+            cy.get(".mat-option").contains("Mother's maiden name?").click();
+            cy.get("#securityAnswerControl").type(maidenName);
+            cy.get("#registerButton").click();
+            cy.get(".mat-simple-snackbar").contains("Registration completed successfully");
+        });
+    
+        it("login from new user", () => {
+            cy.get("#email").type(email);
+            cy.get("#password").type(password);
+            cy.get("#loginButton").click();
+            cy.get(".mat-button-wrapper").contains("Your Basket").click();
+            cy.get("h1").contains(email);
+        });
     })
+    
+    describe("API tests", () => {
 
-    it("Sign up new user", () => {
-        cy.get("#newCustomerLink").click();
-        cy.get("#emailControl").type(email);
-        cy.get("#passwordControl").type(password);
-        cy.get("#repeatPasswordControl").type(password);
-        cy.get(".mat-select-placeholder").click();
-        cy.get(".mat-option").contains("Mother's maiden name?").click();
-        cy.get("#securityAnswerControl").type(maidenName);
-        cy.get("#registerButton").click();
-        cy.get(".mat-simple-snackbar").contains("Registration completed successfully");
-    });
+        const body = {
+            "email": email, 
+            "password": password
+        }
 
-    it("login from new user", () => {
-        cy.get("#email").type(email);
-        cy.get("#password").type(password);
-        cy.get("#loginButton").click();
-        cy.get(".mat-button-wrapper").contains("Your Basket").click();
-        cy.get("h1").contains(email);
-    });
+        it("Test login via API test", () => {
+            cy.request({
+                method: "POST",
+                url: "http://localhost:3000/rest/user/login",
+                body
+            }).then(response => {
+                expect(response.status).to.eql(200);
+            })
+        });
+
+        it("Login via Token non UI", () => {
+            cy.request({
+                method: "POST",
+                url: "http://localhost:3000/rest/user/login",
+                body
+            }).then(response => {
+                const token = response.body.authentication.token;
+                cy.log(token);
+
+                 cy.visit("http://localhost:3000", {
+                     //do following before load url
+                     onBeforeLoad(browser) {
+                         browser.localStorage.setItem("token", token)
+                     }
+                 })
+                cy.get(".cdk-overlay-backdrop").click({ force: true });
+                cy.get(".fa-layers-counter").contains(0);
+            })
+        })
+    })
 })
